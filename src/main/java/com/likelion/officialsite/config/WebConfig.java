27c3d5ace1a97ef2c01ca93b63f7028ebf530474
jeongju <wnwjdqkr@gmail.com>
2025-02-16 23:12:10 +0900

    fix

--- src/main/java/com/likelion/officialsite/config/WebConfig.java
+++ src/main/java/com/likelion/officialsite/config/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")  // 모든 경로에 대해 CORS 허용
-                        .allowedOrigins("http://localhost:3000", "https://likelionsg13.site")  // 허용할 도메인
+                        .allowedOrigins("http://localhost:3000", "http://127.0.0.1:3000", "https://likelionsg13.site")  // 허용할 도메인
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
